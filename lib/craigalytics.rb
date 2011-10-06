86d8c04e4911318cf91e83a1eb1b00b1db7912e2
1337807 <jonan.scheffler@g5platform.com>
2011-10-05 21:12:08 -0700

    Setup testing

--- lib/craigalytics.rb
+++ lib/craigalytics.rb
@@ -1,4 +1,6 @@
 require "craigalytics/version"
 
 module Craigalytics
+  class PostCollection
+  end
 end
---
+++ spec/craigalytics_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+module Craigalytics
+  describe PostCollection do
+    describe "#update" do
+    end
+
+    describe "#count" do
+    end
+  end
+end
